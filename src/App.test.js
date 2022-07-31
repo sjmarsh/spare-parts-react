4a64185ffb73d8a260cde19d5b08edcbba7b185d
sjmarsh <sjmarsh31@gmail.com>
2022-07-31 15:19:53 +1000

    Add tests for partDetailSchema

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders About link', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/About/i)).toBeInTheDocument();
 });
---
+++ src/features/parts/tests/partDetailShema.spec.js
@@ -0,0 +1,67 @@
+import PartDetailSchema from '../partDetailSchema';
+
+const getTestPart = () => {
+    return {
+        name: "name",
+        description: "description", 
+        weight: 1.1,
+        price: 2.2,
+        startDate: '2020-12-31',
+        endDate: '2022-12-31'
+    }
+}
+
+test('has no errors when validation criteria met', async () => {
+    const part = getTestPart();
+    const result = await PartDetailSchema.isValid(part);
+    expect(result).toEqual(true);
+})
+
+test('has error when name is blank', async () => {
+    const part = getTestPart();
+    part.name = null;
+    const result = await PartDetailSchema.isValid(part);
+    expect(result).toEqual(false);
+})
+
+test('has error when weight is blank', async () => {
+    const part = getTestPart();
+    part.weight = null;
+    const result = await PartDetailSchema.isValid(part);
+    expect(result).toEqual(false);
+})
+
+test('has error when weight less than zero', async () => {
+    const part = getTestPart();
+    part.weight = -1;
+    const result = await PartDetailSchema.isValid(part);
+    expect(result).toEqual(false);
+})
+
+test('has error when price is blank', async () => {
+    const part = getTestPart();
+    part.price = null;
+    const result = await PartDetailSchema.isValid(part);
+    expect(result).toEqual(false);
+})
+
+test('has error when price is less than zero', async () => {
+    const part = getTestPart();
+    part.price = -0.1;
+    const result = await PartDetailSchema.isValid(part);
+    expect(result).toEqual(false);
+})
+
+test('has error when start date is blank', async () => {
+    const part = getTestPart();
+    part.startDate = null;
+    const result = await PartDetailSchema.isValid(part);
+    expect(result).toEqual(false);
+})
+
+test('has error when start date is before 2000', async () => {
+    const part = getTestPart();
+    part.startDate = '1999-12-31';
+    const result = await PartDetailSchema.isValid(part);
+    expect(result).toEqual(false);
+})
