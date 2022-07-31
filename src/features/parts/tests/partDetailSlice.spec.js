cdade40a203176a323cd8ad08bd7acf6015fefcb
sjmarsh <sjmarsh31@gmail.com>
2022-07-31 17:49:01 +1000

    Add some tests for fetchPart

--- src/features/parts/tests/partDetailSlice.spec.js
+++ src/features/parts/tests/partDetailSlice.spec.js
@@ -1,7 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
 import partDetailReducer, {
     showDetail, 
     fetchPart
 } from '../partDetailSlice';
+import { client } from '../../../api/client';
 
 import DetailMode from '../../../app/constants/detailMode';
 import FetchStatus from '../../../app/constants/fetchStatus';
@@ -14,6 +16,10 @@ describe('partDetail reducer', () => {
       status: FetchStatus.Idle,
       error: null
     };
+
+    afterEach(() => {
+        jest.resetAllMocks();
+    })
 
     it('should handle initial state', () => {
         expect(partDetailReducer(undefined, { type: 'unknown' })).toEqual({
@@ -31,4 +37,48 @@ describe('partDetail reducer', () => {
         expect(actual.id).toEqual(1);
     });
 
+    it('should fetch empty part when part id is zero', async () => {
+        const partId = 0;
+        const store = configureStore({reducer: partDetailReducer});
+        await store.dispatch(fetchPart(partId)); 
+        expect(store.getState()).toEqual({
+            error: null, 
+            id: 0, 
+            mode: DetailMode.Closed, 
+            status: FetchStatus.Succeeded, 
+            value: { id: 0, name: "", description: "", weight: 0, price: 0, startDate: "", endDate: null }
+        });
+    })
+
+    it('should handle successful fetch part', async () => {
+        const partId = 1;
+        const fakeResponse = { data: { value: { id: partId }}};
+        jest.spyOn(client, 'get').mockResolvedValueOnce(fakeResponse);
+        const store = configureStore({reducer: partDetailReducer});
+        await store.dispatch(fetchPart(partId));
+        expect(store.getState()).toEqual({
+            error: null, 
+            id: 0, 
+            mode: DetailMode.Closed, 
+            status: FetchStatus.Succeeded, 
+            value: {id: partId}
+        });
+    });
+
+    it('should handle erroneous fetch part', async () => {
+        const partId = 1;
+        const errorMessage = 'Oh no it all went wrong!';
+        jest.spyOn(client, 'get').mockRejectedValueOnce(new Error(errorMessage));
+        const store = configureStore({reducer: partDetailReducer});
+        await store.dispatch(fetchPart(partId));
+        expect(store.getState()).toEqual({
+            error: errorMessage, 
+            id: 0, 
+            mode: DetailMode.Closed, 
+            status: FetchStatus.Failed, 
+            value: {}
+        });
+    });
+
+    // TODO: create and update part tests
 });
