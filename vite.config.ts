5d3bfb2bb9beacb789fd6b5615f2a6eb901c2dba
sjmarsh <sjmarsh31@gmail.com>
2022-10-04 12:05:33 +1100

    Update vite dev server config to open browser.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from 'fs';
+import dns from 'dns';
+
+dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
   server: {
     host: 'localhost',
     port: 3000,
+    open: true,
     https: {
       key: fs.readFileSync('../reactcert/key.pem'),
       cert: fs.readFileSync('../reactcert/cert.pem')
